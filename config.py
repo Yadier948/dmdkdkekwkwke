78abe7179e90ea44a20e209bffe22f741b48e67b
Yadier948 <117450763+Yadier948@users.noreply.github.com>
2022-11-14 08:11:29 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN =  os.environ.get('bot_token','5785838098:AAFg_6MqOk4b-lJDotkyb0CHIllluhtL9lM')
+BOT_TOKEN =  os.environ.get('bot_token','5325291385:AAHWftRu9ikAjwi7HBlXhW9VL2jk8vJcAYM')
 API_ID =  os.environ.get('api_id','18641760')
 API_HASH = os.environ.get('api_hash','b7b026ce9d1d36400c02dc21d8df53a3')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','Elpatron220').split(';')
-#ACCES_USERS = ('tl_admin_user','Elpatron220')
+ACCES_USERS = os.environ.get('tl_admin_user','countryandlifee').split(';')
+#ACCES_USERS = ('tl_admin_user','countryandlifee')
 #ACCES_USERS = os.environ.get(ACCES_USERS)
 PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KFGGJJYGJKLHFKYEDIGDYJJEKEFKREEGLFDKLH'))
 
