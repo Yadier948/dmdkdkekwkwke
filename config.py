165cd5ccbea1592d62d29a01fda3a290f14e7a21
Yadier948 <117450763+Yadier948@users.noreply.github.com>
2022-11-14 08:17:04 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ API_ID =  os.environ.get('api_id','18641760')
 API_HASH = os.environ.get('api_hash','b7b026ce9d1d36400c02dc21d8df53a3')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','countryandlifee').split(';')
+ACCES_USERS = os.environ.get('tl_admin_user','countryandlifee').split(';countryandlifee')
 #ACCES_USERS = ('tl_admin_user','countryandlifee')
-#ACCES_USERS = os.environ.get(ACCES_USERS)
+#ACCES_USERS = os.environ.get(countryandlifee)
 PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KFGGJJYGJKLHFKYEDIGDYJJEKEFKREEGLFDKLH'))
 
 if PROXY:
